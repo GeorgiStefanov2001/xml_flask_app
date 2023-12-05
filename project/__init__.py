916677622635c7d4ad70e820befad454460964c8
George Stefanov <georgi.stefanov2001@gmail.com>
2023-12-05 20:51:07 +0200

    feat: Add user login and user session

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -17,7 +18,15 @@ def create_app():
 
     db.init_app(app)
 
-    from . import models
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     with app.app_context():
         db.create_all()
--- project/auth.py
+++ project/auth.py
@@ -1,5 +1,6 @@
 import bcrypt
 from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask_login import login_required, login_user, logout_user
 
 from . import db
 from .models import User
@@ -12,6 +13,33 @@ def login():
     return render_template("login.html")
 
 
+@auth.route("/login", methods=["POST"])
+def login_post():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    user = User.query.filter_by(username=username).first()
+
+    flash_msg = ""
+    if not user:
+        flash_msg = "Invalid username - user doesn't exist."
+    else:
+        # check if password is correct
+        hashed_pass = user.password
+        password_correct = (
+            bcrypt.hashpw(bytes(password, "utf-8"), hashed_pass) == hashed_pass
+        )
+        flash_msg = flash_msg if password_correct else "Incorrect password."
+
+    if flash_msg != "":
+        flash(flash_msg)
+        return redirect(url_for("auth.login"))
+
+    # credentials are correct - log user in
+    login_user(user, remember=(True if request.form.get("remember") else False))
+    return redirect(url_for("main.profile"))
+
+
 @auth.route("/signup")
 def signup():
     return render_template("signup.html")
@@ -20,22 +48,26 @@ def signup():
 @auth.route("/signup", methods=["POST"])
 def signup_post():
     email = request.form.get("email")
+    username = request.form.get("username")
 
-    user = User.query.filter_by(
-        email=email
-    ).first()  # if this returns a user, then the email already exists in database
+    user = User.query.filter_by(email=email).first()
 
     # if a user is found, email already exists in the db
     # redirect back to signup page so user can try again
 
     if user:
-        flash("Email address already exists.")
+        flash("Email address already exists")
         return redirect(url_for("auth.signup"))
+    else:
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash("Username already exists")
+            return redirect(url_for("auth.signup"))
 
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
     new_user = User(
         email=email,
-        username=request.form.get("username"),
+        username=username,
         password=bcrypt.hashpw(
             bytes(request.form.get("password"), "utf-8"), bcrypt.gensalt()
         ),
@@ -50,5 +82,7 @@ def signup_post():
 
 
 @auth.route("/logout")
+@login_required
 def logout():
-    return "Logout"
+    logout_user()
+    return redirect(url_for("main.index"))
